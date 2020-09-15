4b518e0c21239d2c4242dcb4244466e28ef733d5
unknown <ahmed.hadjsaad@iit.ens.tn>
2020-09-15 11:57:29 +0200

    Updates

---
+++ precache-manifest.bd15addaf1ebfbdad1fe25b81fe62f33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d8edd3c37eab8d5f281c3ab54744ea",
+    "url": "/React-checkpoint/index.html"
+  },
+  {
+    "revision": "3b25f78820127a063a39",
+    "url": "/React-checkpoint/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b7e690423a8a4bbd0fcf",
+    "url": "/React-checkpoint/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "3b25f78820127a063a39",
+    "url": "/React-checkpoint/static/js/2.b5717cc5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-checkpoint/static/js/2.b5717cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7e690423a8a4bbd0fcf",
+    "url": "/React-checkpoint/static/js/main.8e71b052.chunk.js"
+  },
+  {
+    "revision": "3fb4320e013eda4e94d0",
+    "url": "/React-checkpoint/static/js/runtime-main.be5a6d46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-checkpoint/static/media/logo.5d5d9eef.svg"
+  }
+]);
